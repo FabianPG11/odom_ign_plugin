810fac565f520b7ec4b5ad6993e6752cad94d9ec
FabianPG11 <parbian_11@hotmail.com>
2020-12-07 11:29:02 +0200

    Odom plugin

---
+++ build/CMakeFiles/OdomOmnidirectional.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fabian/robotont_ws/src/robotont_gazebo/ign_worlds/odom_ign_plugin/OdomOmnidirectional.cc" "/home/fabian/robotont_ws/src/robotont_gazebo/ign_worlds/odom_ign_plugin/build/CMakeFiles/OdomOmnidirectional.dir/OdomOmnidirectional.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_QML_LIB"
+  "QT_QUICKCONTROLS2_LIB"
+  "QT_QUICK_LIB"
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"ign_plugins\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/ros/melodic/include"
+  "/opt/ros/melodic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/usr/include/ignition/plugin1"
+  "/usr/include/ignition/cmake2"
+  "/usr/include/ignition/gazebo3"
+  "/usr/include/ignition/math6"
+  "/usr/include/ignition/common3"
+  "/usr/include/uuid"
+  "/usr/include/ignition/fuel_tools4"
+  "/usr/include/ignition/msgs5"
+  "/usr/include/eigen3"
+  "/usr/include/ignition/gui3"
+  "/usr/include/x86_64-linux-gnu"
+  "/usr/include/ignition/transport8"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQml"
+  "/usr/include/x86_64-linux-gnu/qt5/QtNetwork"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQuick"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQuickControls2"
+  "/usr/include/sdformat-9.3/sdf/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/OdomOmnidirectional.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OdomOmnidirectional.dir/OdomOmnidirectional.cc.o"
+  "libOdomOmnidirectional.pdb"
+  "libOdomOmnidirectional.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OdomOmnidirectional.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
